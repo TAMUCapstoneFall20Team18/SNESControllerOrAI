7965a89aa45eb147870e07113d0b2182e342cabb
Andrew Frosch <froscand@tamu.edu>
2021-01-23 15:27:13 -0600

    Update controllerServer.py

--- BlueToothCommunications/controllerServer.py
+++ BlueToothCommunications/controllerServer.py
@@ -1,6 +1,7 @@
 import socket
 
 hostMACAddress = '54:13:79:6D:CE:B6'
+msg = "5"
 port = 5
 backlog = 1
 size = 1024
@@ -8,12 +9,13 @@ s = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM
 s.bind((hostMACAddress, port))
 s.listen(backlog)
 try:
-	client, clientInfo = s.accept()
+	client, address = s.accept()
 	while 1:
 		data = client.recv(size)
 		if data:
 			print(data)
-			client.send(data)
+			# This can be any data:
+			client.send(bytes(msg, 'UTF-8'))
 except:
 	print("Closing socket")
 	client.close()

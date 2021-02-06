319cf4c2c1207c87200c09fff90a1131d7491204
Jacob Widman <jacob.l.widman@gmail.com>
2021-02-06 15:05:17 -0600

    Add files via upload

--- Intergration/Pipe_socket.py
+++ Intergration/Pipe_socket.py
@@ -8,20 +8,14 @@
 ##This manual may need a secondary port to listen to. This will complicate things
 ##   as I will need the manual to send a message when changed and have listening to that
 ##This will be fun.
-##for later more general uses, screenshot function, RM stays, but everything else
-#chagne
 
 ##2021-01-23
 ##CHANGES Changed to allow for a random amout of change to the keypress
 import time
 import socket
 import random
+import re
 
-##HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-##PORT_RECEIVE = 9084     # Port to listen on (non-privileged ports are > 1023)
-##DAEMON_NAME  = "Pipe_socket"
-### f = open("./byuu.simulated_keypresses.txt","ab")
-##
 ##def setup_sockets():
 ##    global s_receive
 ##    s_receive = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -38,12 +32,20 @@ import random
 ##    print(f"Closed sockets {DAEMON_NAME}")
 ##
 hostMACAddress = 'BC:14:EF:A3:39:3C'
-recieve_port = 6 ##bluetooth for RM socket
+recieve_port = 5 ##bluetooth for RM socket ##This is becuase currently looking at one port
 backlog = 1
 size = 1024 ##size of the buffer
-s_blue = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
-s_blue.bind((hostMACAddress, recieve_port))
 
+def socket_setup(): ##Sets up the bluetooth socket
+    global s_blue
+    s_blue = socket.socket(socket.AF_BLUETOOTH, socket.SOCK_STREAM, socket.BTPROTO_RFCOMM)
+    s_blue.bind((hostMACAddress, recieve_port))
+
+def close_socket(): ##final closing socket
+    global s_blue, s_receive
+    s_receive.shutdown(socket.SHUT_RDWR)
+    s_receive.close()
+    print(f"Closed sockets pipe_socket")
 
 def generate_rand_pulse():
     rand_selector = random.randint(0, 1000)
@@ -54,13 +56,9 @@ def generate_rand_pulse():
 
 def main():
     #global s_receive, s_send
+    global s_blue, s_receive
     f = open("/home/widman/Documents/School20Fall/ECEN_403/retro_gym/higan-nightly/higan-nightly/myfifo","wb")
-    ##Will need to be edited in 404   
-    #if ai_selector == 1:
     
-    #at full speed this indicator could be the logging function
-    f.write(b'1') ## to initiate switch to AI from manual play (Changes speed)
-    f.flush()
     while True:
 ##        s_receive.listen()
 ##        conn, addr = s_receive.accept()
@@ -70,47 +68,67 @@ def main():
 ##            if not data:
 ##                keypress = ''
 ##                break
+        p1 = re.compile("^\d{3}") #assuming that the 16 vector is longer than 3 digits
+        p2 = re.compile("^\[atf]{1}")
+        p3 = re.compile("^\d{1,2}")
+        ##button press for manual
+        #key; a is button press, t is AI on, f is AI off
+        
+        s_receive, address = s_blue.accept()
         s_blue.listen(backlog)
+        
         try:
-            
-            s_receive, address = s_blue.accept()
-            m.
-            while 1:
-                    data = s_receive.recv(size)
+            while True:
+                data = s_receive.recv(size)
+                if not data:
+                    keypress = ''
+                    break
+                keypress = data.decode()
+                print(f"This is the data receieved {data}")
+                m1 = p1.match(data)
+                m2 = p2.match(data)
+                m3 = p3.match(data)
+                if m1: #this is if 16 bit vector
+                    keypress = ''
+                    print(f"16 vector")
+                    break
+                if m2:
+                    print("From Pi {}")
+                    if data == 't':
+                        f.write(b'1')
+                        f.flush()
+                    if data == 'f':
+                        f.write(b'2')
+                        f.flush()
+                    elif data == 'a':
+                        f.write(b'a')
+                        f.flush()                          
+
+                if m3:
+                    keypress = int(data.decode())
+                    add_pulse = generate_rand_pulse()
+                    if add_pulse == True:
+                        keypress += 1
+                    print(f"Input to pipe is {keypress} presses")
                     
-                    if not data:
-                        keypress = ''
-                        break
+                    while keypress > 0: ##This needs the delay pulse
+                        f.write(b'a')
+                        f.flush()
+                        time.sleep(0.1)
+                        keypress -= 1
+
+                    
+                    
     
         except:
             print("Closing socket")
             s_receive.close()
             s_blue.close()
-
-            keypress = int(data.decode())
-
-
-            
-        add_pulse = generate_rand_pulse()
-        if add_pulse == True:
-            keypress += 1
-        print(f"Input to pipe is {keypress} presses")
-        ##Have a ignore switch for
-        
-        while keypress > 0: ##This needs the delay pulse
-            f.write(b'a')
-            f.flush()
-            time.sleep(0.1)
-            keypress -= 1
         
     f.close()
 
 try:
- #   setup_sockets()
+    socket_setup()
     main()
 finally:
-    f = open("/home/widman/Documents/School20Fall/ECEN_403/retro_gym/higan-nightly/higan-nightly/myfifo","wb")
-    f.write(b'2')
-    f.flush()
-    f.close()
     closesocket()
